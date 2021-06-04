25070cdb94f2876dd1fe81f92cb4bbf56f0cd437
Jack Devey <jackdevey@protonmail.com>
2021-06-04 07:50:43 +0100

    Update header-writer.py

--- scripts/header-writer.py
+++ scripts/header-writer.py
@@ -3,5 +3,5 @@ import os
 files = os.listdir('/home/runner/work/Lux/Lux')
 
 for file in files:
-    if file.startswith("/pkg") OR NOT file.endswith('.go'):
+    if file.startswith("/pkg") or not file.endswith('.go'):
         print("Excluded: " + file)
