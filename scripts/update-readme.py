890b358d73309d0a6bf48ce290a2115286e9e5d9
Jack Devey <jackdevey@protonmail.com>
2021-06-04 06:31:57 +0100

    Update update-readme.py

--- scripts/update-readme.py
+++ scripts/update-readme.py
@@ -3,7 +3,7 @@ import json
 
 url = "https://bandev.uk/api/lux/commands.json"
 
-data = json.loads(requests.get(url))
+data = json.loads(requests.get(url).text)
 
 for command in data["commands"]:
   print(command["name"])
