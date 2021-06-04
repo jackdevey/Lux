1f6ae6089c974ec6f6909e2be225df175865b394
Jack Devey <jackdevey@protonmail.com>
2021-06-04 06:56:50 +0100

    Update update-readme.py

--- scripts/update-readme.py
+++ scripts/update-readme.py
@@ -9,7 +9,7 @@ readme = open("/home/runner/work/Lux/Lux/README.md", "r").read()
 
 a1 = readme.split("<ul id='EDTCMDS'>")[1]
 a2 = a1.split("</ul>")[0]
-a3 = a2.split("<li>")
+commands = a2.split("<li>")
 
 i = 0
 
