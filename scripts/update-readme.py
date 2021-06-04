e07a155cd992ffee8baad39256ef29a924013204
Jack Devey <jackdevey@protonmail.com>
2021-06-04 06:43:16 +0100

    Update update-readme.py

--- scripts/update-readme.py
+++ scripts/update-readme.py
@@ -5,5 +5,13 @@ url = "https://bandev.uk/api/lux/commands.json"
 
 data = json.loads(requests.get(url).text)
 
+readme = open("readme.md", "r").read()
+
+commands = readme.split("<ul id='EDTCMDS'>")[0].split("</ul>")[0].split("<li>")
+
+i = 0
+
 for command in data["commands"]:
-  print(command["name"])
+  commands[i] = "<code>"+command["examples"][0]+"</code><p>"+command["description"]+"</p>"
+  print(commands[i])
+  i = i + 1
