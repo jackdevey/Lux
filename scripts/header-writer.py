2e4bf74adbf348aa1270561692e1843523f68607
Jack Devey <jackdevey@protonmail.com>
2021-06-04 08:08:16 +0100

    Update header-writer.py

--- scripts/header-writer.py
+++ scripts/header-writer.py
@@ -1,9 +1,33 @@
 import glob
 
-files = glob.glob('/home/runner/work/Lux/Lux/*/*/*.go')
+files_tmp = glob.glob('/home/runner/work/Lux/Lux/*/*/*.go')
 
-for file in files:
-    if file.startswith("/pkg"):
-        print("Excluded: " + file)
+for file_tmp in files_tmp:
+    if file_tmp.startswith("/home/runner/work/Lux/Lux/pkg"):
+        print("Excluded: " + file_tmp)
     else:
-        print("Accepted: " + file)
+        print("Accepted: " + file_tmp)
+        files.append(file_tmp)
+        
+for file in files:
+    text = open(file).read()
+    if text.startswith("""/**
+
+Lux
+Copyright (C) 2021  BanDev
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/"""):
+        print("Header found")
